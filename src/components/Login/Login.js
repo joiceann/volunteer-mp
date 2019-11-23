bfdc86be0ba7e9f982da53495dfd0b39830d4d7b
Carlosc23 <cal15219@uvg.edu.gt>
2019-11-22 18:14:16 -0600

    feat: add register link

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,17 @@
-import React, {
-  useState,
-} from 'react';
-import Container from '@material-ui/core/Container';
-import Paper from '@material-ui/core/Paper';
-import colors from '../../colors';
-import styled, { createGlobalStyle } from 'styled-components';
-import Typography from '@material-ui/core/Typography';
-import { Button, TextField } from '../CommonComponents';
-import Avatar from '@material-ui/core/Avatar';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import * as Constants from '../../constants';
-import axios from '../../axios';
-import { useHistory } from 'react-router-dom';
-import {
-  LoadingScreen,
-} from '../CommonComponents';
+import React, { useState } from "react";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import colors from "../../colors";
+import styled, { createGlobalStyle } from "styled-components";
+import Typography from "@material-ui/core/Typography";
+import { Button, TextField } from "../CommonComponents";
+import Avatar from "@material-ui/core/Avatar";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import * as Constants from "../../constants";
+import axios from "../../axios";
+import { useHistory } from "react-router-dom";
+import { LoadingScreen } from "../CommonComponents";
+import { Link } from 'react-router-dom';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -39,8 +36,21 @@ const StyledPaper = styled(Paper)`
   padding: 30px;
 `;
 
+const StyledPaper2 = styled(Paper)`
+  position: relative;
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 30px;
+  margin-top: 160%;
+  margin-left: -100%
+
+`;
+
 const Logo = styled.h1`
-  font-family: 'Josefin Sans', sans-serif;
+  font-family: "Josefin Sans", sans-serif;
   user-select: none;
   margin-right: 8px;
 `;
@@ -63,12 +73,11 @@ const Form = styled.form`
 //{8,}
 const generalInputRegex = /^[a-zA-Z0-9¡¿?@.:+]*$/gim;
 const emailRegex = /^([\w.%+-]+)@([\w-])+(\.+[\w]{2,})*$/gim;
-const credentialRegex  = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[¡¿?@:+]).{8,}$/gim;
+const credentialRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[¡¿?@:+]).{8,}$/gim;
 
 const Login = ({ login }) => {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [formError, showFormError] = useState(false);
   const [isLoading, setLoading] = useState(false);
   const [validEmail, setValidEmail] = useState(false);
@@ -79,9 +88,7 @@ const Login = ({ login }) => {
   const [validPasswordText, setPT] = useState("");
   const [validTriesText, setTT] = useState("");
 
-
-
-  const handleEmailChange = (event) => {
+  const handleEmailChange = event => {
     console.log(event.target.value);
     let emailValid = event.target.value.match(emailRegex);
     let inputValid = event.target.value.match(generalInputRegex);
@@ -91,73 +98,66 @@ const Login = ({ login }) => {
     console.log(emailValid);
     console.log(inputValid);
 
-    if (reeval){
+    if (reeval) {
       setET("");
+    } else {
+      setET("Ingreso de texto en correo inválido\n");
     }
-    else{
-      setET("Ingreso de texto en correo inválido\n");    }
     setEmail(event.target.value);
-
   };
 
-  const handlePasswordChange = (event) => {
+  const handlePasswordChange = event => {
     let passwordValidate = event.target.value.match(credentialRegex);
     setValidPassword(passwordValidate != null);
-    if (passwordValidate != null){
+    if (passwordValidate != null) {
       setPT("");
-    }
-    else{
+    } else {
       setPT("Ingreso de texto en correo inválido\n");
     }
     setPassword(event.target.value);
   };
 
-  const submit = (event) => {
+  const submit = event => {
     event.preventDefault();
     setLoading(true);
-    if ({validEmail} && {validPassword} || 4 < {validTries}){
-      axios.post(Constants.LOGIN,
-          JSON.stringify({email, password}),
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          },
-      ).then((response) => {
-        console.log('user logged in: ', response)
-        login(response.data.token, response.data.type);
-        setLoading(false);
-        history.push('/dashboard/my-projects');
-      }).catch((response) => {
-        console.log(response);
-        if (response.data == "Invalid Credentials") {
-            setValidTries({validTries} + 1);
-        }
-        showFormError(true);
-        setLoading(false);
-      });
-    }
-    else{
-        if (4 < {validTries}) {
-            setTT("Se alcanzó el maximo de intentos permitido");
-        }
-        showFormError(true);
-        setLoading(false);
+    if (({ validEmail } && { validPassword }) || 4 < { validTries }) {
+      axios
+        .post(Constants.LOGIN, JSON.stringify({ email, password }), {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => {
+          console.log("user logged in: ", response);
+          login(response.data.token, response.data.type);
+          setLoading(false);
+          history.push("/dashboard/my-projects");
+        })
+        .catch(response => {
+          console.log(response);
+          if (response.data == "Invalid Credentials") {
+            setValidTries({ validTries } + 1);
+          }
+          showFormError(true);
+          setLoading(false);
+        });
+    } else {
+      if (4 < { validTries }) {
+        setTT("Se alcanzó el maximo de intentos permitido");
+      }
+      showFormError(true);
+      setLoading(false);
     }
   };
   return (
     <MainContainer maxWidth="xs">
       <GlobalStyle />
       <StyledPaper>
-        <Logo>
-          Voluntourist
-        </Logo>
+        <Logo>Voluntourist</Logo>
         <CustomAvatar>
           <LockOutlinedIcon />
         </CustomAvatar>
-        <Typography variant="h5">
-          Inicio de sesión
-        </Typography>
+        <Typography variant="h5">Inicio de sesión</Typography>
         <Form noValidate onSubmit={submit}>
           <TextField
             variant="outlined"
@@ -185,16 +185,14 @@ const Login = ({ login }) => {
             id="password"
             autoComplete="current-password"
           />
-          {
-            formError &&
+          {formError && (
             <Typography color="error">
-              Correo electrónico o contraseña incorrectos {validEmailText} {validPasswordText}
+              Correo electrónico o contraseña incorrectos {validEmailText}{" "}
+              {validPasswordText}
             </Typography>
-          }
+          )}
 
-          {
-            isLoading && <LoadingScreen dark="true" />
-          }
+          {isLoading && <LoadingScreen dark="true" />}
           <SeparatedButton
             type="submit"
             fullWidth
@@ -205,7 +203,13 @@ const Login = ({ login }) => {
           </SeparatedButton>
         </Form>
       </StyledPaper>
+      
+      <StyledPaper2>
+      <Typography variant="h6">¿Quieres ser voluntario?</Typography>
+      <Typography variant="h6"> <Link to="/Register" >Crea una cuenta</Link></Typography>
+      </StyledPaper2>
     </MainContainer>
+    
   );
 };
 
