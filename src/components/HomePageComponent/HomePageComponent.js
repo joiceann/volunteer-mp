e9ee906147c79f52055698fd3fa1f9543849d3b2
CoffeeBrewer <cas15151@uvg.edu.gt>
2019-11-24 16:18:56 -0600

    Plantilla inicial de Landing

--- src/components/HomePageComponent/HomePageComponent.js
+++ src/components/HomePageComponent/HomePageComponent.js
@@ -1,4 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import styled from "styled-components";
+
+const Logo = styled.h1`
+  font-family: 'Josefin Sans', sans-serif;
+  user-select: none;
+  margin-right: 8px;
+  margin-top: 10px;
+`;
 
 export default class HomePageComponent extends Component {
     constructor(props) {
@@ -9,7 +17,34 @@ export default class HomePageComponent extends Component {
 
     render = () => {
         return(
-            <div>Home</div>
+            <div>
+                <div style={{height: '300px', position: 'relative'}}>
+                    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+                        <Header transparent >
+                            <Logo>Voluntourist</Logo>
+                            <Navigation>
+                                <a href="/login">Login</a>
+                                <a href="/register">Register</a>
+                            </Navigation>
+                        </Header>
+                    </Layout>
+                </div>
+                <div>
+                    <Content>git 
+                    </Content>
+                </div>
+                <div>
+                    <Footer size="mini">
+                        <FooterSection type="left" logo="Title">
+                            <FooterLinkList>
+                                <a href="#">Help</a>
+                                <a href="#">Privacy & Terms</a>
+                            </FooterLinkList>
+                        </FooterSection>
+                    </Footer>
+
+                </div>
+            </div>
         )
     }
 }
