fc8c7716d8fd37140b74de7c2069dc66ff0c6a37
Carlosc23 <cal15219@uvg.edu.gt>
2019-11-24 17:23:05 -0600

    add register route to constants

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 export const BASE_URL = 'http://voluntariosapiweb-dev.pxvnantpm2.us-west-2.elasticbeanstalk.com/api/v1';
 
 export const LOGIN = '/users/login';
+export const REGISTER = '/users/register';
 export const PROJECTS_LIST = '/projects/list';
 export const UPDATE_ROLE = '/projects/updaterole'
 export const ENROLL = '/projects/enroll'
