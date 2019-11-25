17b82c5a7cc019f8005e243ce880a44689224257
CoffeeBrewer <cas15151@uvg.edu.gt>
2019-11-25 04:29:20 -0600

    Landing Page terminada

--- src/components/HomePageComponent/HomePageComponent.js
+++ src/components/HomePageComponent/HomePageComponent.js
@@ -56,7 +56,55 @@ export default class HomePageComponent extends Component {
                         </Header>
                     </Layout>
                 </div>
-                <div style={{ filter: 'brightness(50%)', height: '750px', width: '100%', background: 'url(https://voluntaris-images.s3-us-west-2.amazonaws.com/Grupo2.jpg) center / cover'}}>
+                <div style={{ height: '750px', width: '100%', background: 'url(https://voluntaris-images.s3-us-west-2.amazonaws.com/Grupo2.jpg) center / cover'}}>
+                    <Card shadow={0}  style={{backgroundColor: 'rgb(0,0,0)',opacity:'0.6',  height: '60%', width: '80%', position:'absolute',  left:'10%' , top:'40%', fontFamily: 'Josefin Sans'}}>
+                        <CardText style={{fontFamily: 'Josefin Sans', color: 'white', fontSize: '80px', backgroundColor: 'rgb(0,0,0)', textAlign: 'right', top: '30%'}}>
+                            Who are we?
+                        </CardText>
+                        <CardText style={{color: 'white', fontSize: '45px', fontFamily: 'Josefin Sans'}}>
+                            We are:
+                        </CardText>
+                        {/*<CardText style={{padding: '0',  width: '100%', height: '5%',fontfamily: 'Josefin Sans', color: 'white'}}>*/}
+                        {/*    French Guatemalan Irish Argentinean Czech Chinese Welsh Russian Cuban Syrian Indian Thai Portuguese Chilean*/}
+                        {/*</CardText>*/}
+                        {/*<CardText style={{padding: '0',  width: '100%', height: '5%',  fontfamily: 'Josefin Sans', color: 'white'}}>*/}
+                        {/*    German Dutch Taiwanese Swiss Australian Canadian Belgian Costa Rican American Vietnamese Filipino Japanese*/}
+                        {/*</CardText>   */}
+                        <CardText style={{fontfamily: 'Josefin Sans', color: 'white', textAlign: 'justify'}}>
+                            French Guatemalan Irish Argentinean Czech Chinese Welsh Russian Cuban Syrian Indian Thai
+                            Portuguese Chilean German Dutch Taiwanese Swiss Australian Canadian Belgian Costa Rican
+                            American Vietnamese Filipino Japanese Greek Haitian Honduran Paraguayan Egyptian Panamanian
+                            Peruvian Bolivian New Zealander Danish Moroccan Nicaraguan Korean Salvadorian French
+                            Guatemalan Irish Argentinean Czech Hawaiian Chinese Welsh Russian Cuban Syrian Indian Thai Portuguese
+                            Chilean German Dutch Taiwanese Swiss Australian Canadian Belgian Costa Rican American
+                            Vietnamese Filipino Japanese Greek Haitian Honduran Paraguayan Egyptian Panamanian Peruvian
+                            Bolivian New Zealander Danish Moroccan Nicaraguan Korean Salvadorian French Guatemalan Irish
+                            Salvadorian New Guinean Guatemalan Korean
+
+                        </CardText>
+                        <CardText style={{color: 'white', textAlign:'center', fontSize: '90px'}}>
+                            Volunteers
+                        </CardText>
+                        <CardText style={{fontfamily: 'Josefin Sans', color: 'white', textAlign: 'justify'}}>
+                            Argentinean Czech Chinese Welsh Russian Cuban Syrian Indian Thai Portuguese Chilean German
+                            Dutch Taiwanese Swiss Australian Canadian Belgian Costa Rican American Vietnamese Filipino
+                            Japanese Greek Haitian Honduran Paraguayan Egyptian Panamanian Peruvian Bolivian New
+                            Zealander Danish Moroccan Nicaraguan Korean Salvadorian French Guatemalan Irish Argentinean
+                            Czech Chinese Welsh Russian Cuban Syrian Indian Thai Portuguese Chilean German Dutch
+                            Taiwanese Swiss Australian Canadian Belgian Costa Rican American Vietnamese Filipino
+                            Japanese Greek Haitian Honduran Paraguayan Egyptian Panamanian Peruvian Bolivian New
+                            Zealander Danish Moroccan Nicaraguan Korean American
+                        </CardText>
+                        {/*<CardText style={{fontfamily: 'Josefin Sans', color: 'white'}}>*/}
+
+                        {/*</CardText>*/}
+                        {/*<CardText style={{fontfamily: 'Josefin Sans', color: 'white'}}>*/}
+
+                        {/*</CardText>*/}
+                        <CardText style={{textAlign: 'right', color: 'white' }}>
+                            trying to make the world a better place
+                        </CardText>
+                    </Card>
 
                 </div>
                 <div style={{  height: '750px', width: '100%', background: 'url(https://voluntaris-images.s3-us-west-2.amazonaws.com/Granuado.jpg) center / cover'}}>
